b27ae54468237a8da07bb01395036170c7f3aaab
philip borawski <phil@philipborawski.com>
2017-12-29 01:12:41 -0600

    start to define the project

---
+++ qspectro/ui/mainwindow.cpp
@@ -0,0 +1,24 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_actionExit_triggered()
+{
+    QApplication::quit();
+}
+
+void MainWindow::on_actionSDR_toggled(bool arg1)
+{
+
+}
---
+++ qspectro/ui/ui.pri
@@ -0,0 +1,15 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2017-12-29T00:16:55
+#
+#-------------------------------------------------
+
+
+SOURCES += \
+        ui/mainwindow.cpp
+
+HEADERS += \
+        ui/mainwindow.h
+
+FORMS += \
+        ui/mainwindow.ui
